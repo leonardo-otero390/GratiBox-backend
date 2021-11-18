590f48be39630eabd13feba07cd7a4a1f333426f
Leonardo <leonardo.otero390@gmail.com>
2021-11-18 12:51:49 -0400

    create database script

---
+++ src/database/create_db.sql
@@ -0,0 +1,100 @@
+CREATE TABLE "public.users" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.plans" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT "plans_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.user_subscriptions" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"plan_id" integer NOT NULL,
+	"sign_date" DATE NOT NULL,
+	"full_name" varchar(255) NOT NULL,
+	"ship_date" varchar(255) NOT NULL,
+	"ship_info_id" integer NOT NULL,
+	CONSTRAINT "user_subscriptions_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.products" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL UNIQUE,
+	CONSTRAINT "products_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.subscription_products" (
+	"id" serial NOT NULL,
+	"product_id" integer NOT NULL,
+	"subscription_id" integer NOT NULL,
+	CONSTRAINT "subscription_products_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.states" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	CONSTRAINT "states_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.ship_infos" (
+	"id" serial NOT NULL,
+	"ZIP_code" varchar(8) NOT NULL,
+	"address" varchar(255) NOT NULL,
+	"state_id" integer NOT NULL,
+	"city" varchar(255) NOT NULL,
+	CONSTRAINT "ship_infos_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "user_subscriptions" ADD CONSTRAINT "user_subscriptions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "user_subscriptions" ADD CONSTRAINT "user_subscriptions_fk1" FOREIGN KEY ("plan_id") REFERENCES "plans"("id");
+ALTER TABLE "user_subscriptions" ADD CONSTRAINT "user_subscriptions_fk2" FOREIGN KEY ("ship_info_id") REFERENCES "ship_infos"("id");
+
+
+ALTER TABLE "subscription_products" ADD CONSTRAINT "subscription_products_fk0" FOREIGN KEY ("product_id") REFERENCES "products"("id");
+ALTER TABLE "subscription_products" ADD CONSTRAINT "subscription_products_fk1" FOREIGN KEY ("subscription_id") REFERENCES "user_subscriptions"("id");
+
+
+ALTER TABLE "ship_infos" ADD CONSTRAINT "ship_infos_fk0" FOREIGN KEY ("state_id") REFERENCES "states"("id");
+
+
+
+
+
+
+
